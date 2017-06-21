0939d51fb72c21e7b35c621c12c560ffc0a6f2a9
ZurawskyX <renaldi.re28@gmail.com>
2017-06-21 17:20:55 +0700

    tampilan add story

--- app/src/main/java/edu/bluejack16_2/jrpost/NewStoryFragment.java
+++ app/src/main/java/edu/bluejack16_2/jrpost/NewStoryFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,6 +23,9 @@ import edu.bluejack16_2.jrpost.controllers.StoryController;
 public class NewStoryFragment extends Fragment {
 
 
+    EditText txtStory;
+    EditText txtTitle;
+    Button btnSave;
     public NewStoryFragment() {
         //Required empty public constructor
         //ArrayList<String> genres = new ArrayList<>();
@@ -31,8 +39,27 @@ public class NewStoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_story, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_new_story, container, false);
+        try {
+            txtStory = (EditText) view.findViewById(R.id.txtStory);
+            txtTitle = (EditText) view.findViewById(R.id.txtTitle);
+            btnSave = (Button) view.findViewById(R.id.btnSave);
+            btnSave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String story = txtStory.getText().toString();
+                    String title = txtTitle.getText().toString();
+                    if (story.length() == 0) {
+                        Toast.makeText(view.getContext(), "Story must be filled", Toast.LENGTH_SHORT).show();
+                    } else if (title.length() == 0) {
+                        Toast.makeText(view.getContext(), "Title of the Story must be filled", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }catch(Exception e)
+        {
+            Toast.makeText(view.getContext(), e+"", Toast.LENGTH_SHORT).show();
+        }
+        return view;
     }
-
 }
